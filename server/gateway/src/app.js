34bb113affaf69eefc3eee8be55b37e20f3e18e6
Pradeep C <pradeepchandragiri@outlook.com>
2026-05-29 11:09:15 +0530

    fix: update CORS origin to use environment variable for better configuration

--- server/gateway/src/app.js
+++ server/gateway/src/app.js
@@ -11,7 +11,7 @@ import { optionalMiddleware } from './middlewares/optional.middleware.js'
 const app = express()
 app.use(helmet())
 app.use(cookieParser())
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
+app.use(cors({ credentials: true, origin: `${process.env.ORIGIN_URI}` }))
 
 // health check routes
 app.get('/', (req, res) => {
