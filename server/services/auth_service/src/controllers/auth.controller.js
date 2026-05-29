2bb2a2638f1bba7398b73d233842ae87fc77d068
Pradeep C <pradeepchandragiri@outlook.com>
2026-05-29 11:34:07 +0530

    fix: update cookie settings for login to allow cross-site requests

--- server/services/auth_service/src/controllers/auth.controller.js
+++ server/services/auth_service/src/controllers/auth.controller.js
@@ -179,8 +179,8 @@ export const login = async (req, res) => {
 
         res.cookie('accessToken', accessToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 5 * 24 * 60 * 60 * 1000
         })
 
